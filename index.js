28f019e170b40e74f727a8b7b743fc2e10b7c89c
unknown <pqy@seas.upenn.edu>
2020-04-05 20:51:15 -0400

    add deprecated fix

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 
-if (process.env.NODE_ENV !== "production"){
-  require("dotenv").config();
-}
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
 const methodOverride = require("method-override");
 
-const initializePassport = require('./passport-config');
+const initializePassport = require("./passport-config");
 initializePassport(
-  passport, 
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
 const users = [];
@@ -24,13 +21,16 @@ app.use(express.static(__dirname + "/views"));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.set("views", __dirname);
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(flash());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
+console.log(process.env.SESSION_SECRET);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride("_method"));
@@ -59,37 +59,40 @@ var tasks = [
 //middleware to run in post request
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-
 app.get("/", checkAuthenticated, function (req, res) {
-  res.render("views/index", { tasks: tasks});
+  res.render("views/index", { tasks: tasks });
 });
 
-app.get("/login", checkNotAuthenticated, function(req, res) {
+app.get("/login", checkNotAuthenticated, function (req, res) {
   res.render("views/login");
 });
 
-app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: '/login',
-  failureFlash: true
-}));
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-app.get("/register", checkNotAuthenticated, function(req, res) {
+app.get("/register", checkNotAuthenticated, function (req, res) {
   res.render("views/register");
 });
 
-app.post("/register", checkNotAuthenticated, async function(req, res){
+app.post("/register", checkNotAuthenticated, async function (req, res) {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect("/login")
+      password: hashedPassword,
+    });
+    res.redirect("/login");
   } catch {
-    res.redirect('/register')
+    res.redirect("/register");
   }
   console.log(users);
 });
@@ -106,20 +109,20 @@ app.delete("/:task", function (req, res) {
   });
 });
 
-app.delete("/logout", function(req, res) {
+app.delete("/logout", function (req, res) {
   req.logOut();
   res.redirect("/login");
-})
+});
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
-  } 
+  }
   res.redirect("/login");
 }
 
-function checkNotAuthenticated(req, res, next){
-  if (req.isAuthenticated()){
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
     return res.redirect("/");
   }
   next();
