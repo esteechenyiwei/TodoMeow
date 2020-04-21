4a44b09ce8b775084d66716da8b0bcb8263504b6
unknown <pqy@seas.upenn.edu>
2020-04-20 21:27:56 -0400

    fix complete task

--- server.js
+++ server.js
@@ -692,7 +692,7 @@ app.post("/dashboard", urlencodedParser, function (req, res) {
 
 //edit task
 app.get("/task", ensureAuthenticated, function (req, res) {
-  var username = req.body.name;
+  var username = req.user.name;
   var title = req.query.title;
   var desc = req.query.desc;
   var ddl = req.query.deadline;
@@ -714,12 +714,11 @@ app.get("/task", ensureAuthenticated, function (req, res) {
         console.log("no such person, error in getting user info");
       } else {
         console.log("succeeded in display the task");
-        res.render("views/task", { title: title, desc: desc, ddl: ddl });
+        res.render("task", { title: title, desc: desc, ddl: ddl });
       }
     }
   );
 });
-
 app.get("/complete", ensureAuthenticated, (req, res) => {
   var username = req.user.name;
   var title = req.query.title;
